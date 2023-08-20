84eb159c9e6b25ca037929eaf66f47c232fe32fd
hoooooojjjj <rhj080471@gmail.com>
2023-08-20 17:10:18 +0900

    useEffect, API 호출, useMemo 사용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEdtor";
 import DiaryList from "./DiaryList";
-import { useState } from "react";
-import Lifecycle from "./Lifecycle";
+import { useEffect, useMemo, useRef, useState } from "react";
+// import Lifecycle from "./Lifecycle";
+
 function App() {
   const [diaryDatas, setdiaryDatas] = useState([]);
 
-  // const diaryDatasId = useRef(0);
+  const dataId = useRef(0);
+
+  // API를 호출하는 getdata 함수
+  const getdata = async () => {
+    const res = await fetch(
+      // 비동기 함수 fetch()로 API 받아오기
+      "https://jsonplaceholder.typicode.com/comments"
+    )
+      .then((res) => {
+        return res.json(); // API 데이터를 json 형태로 변환
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    // 초기 일기 데이터 세팅
+    const ininData = res.slice(0, 20).map((item) => {
+      // 초기 데이터 배열
+      return {
+        author: item.email,
+        content: item.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setdiaryDatas(ininData); // 초기 데이터 배열을 diaryDatas에 할당
+  };
+
+  useEffect(() => {
+    getdata(); // getdata 함수 호출
+  }, []);
 
   // state 끌어올리기 -> 최상위 컴포넌트에서 이벤트가 발생하면 실행될 '함수'를 만들고 전달,
   // 하위 컴포넌트에서 '이벤트'(->함수)를 현재 데이터를 인자로 전달하여 실행
@@ -46,10 +78,25 @@ function App() {
     setdiaryDatas(editdiaryDatas);
   };
 
+  // useMemo() -> 연산 최적화 = 필요할 때만 연산을 수행하도록 해줌
+  // 감정 비율 분석 -> useMemo 사용 -> 이것은 더 이상 함수가 아님. -> usememo가 리턴하는 값을 그대로 받음.
+  const emotionAnalysis = useMemo(() => {
+    // 첫번째 인자로 콜백함수 -> 콜백함수의 연산을 최적화해줌 -> 콜백함수의 리턴값을 그대로 반환
+    const goodCount = diaryDatas.filter((it) => it.emotion >= 3).length;
+    const sadCount = diaryDatas.length - goodCount;
+    const goodRatio = (goodCount / diaryDatas.length) * 100;
+    return { goodCount, sadCount, goodRatio }; // 리턴 값이 존재해야함
+  }, [diaryDatas.length]); // 두번째 인자로 이 값이 바뀔 때만 콜백함수를 실행시킬 값
+
+  const { goodCount, sadCount, goodRatio } = emotionAnalysis; // 값으로 사용해야함.
   return (
     <div className="App">
-      <Lifecycle />
+      {/* <Lifecycle /> */}
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 개수 : {diaryDatas.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {sadCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}%</div>
       <DiaryList diaryData={diaryDatas} onRemove={onRemove} onEdit={onEdit} />
     </div>
   );
--- src/Lifecycle.js
+++ src/Lifecycle.js
@@ -1,72 +1,72 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-const Unmount = () => {
-  // *** Lifecycle 컴포넌트가 언마운트됐을 때 -> 화면에서 사라졌을 때
-  useEffect(() => {
-    console.log("Mount!");
-    return () => {
-      // 함수를 리턴하면 언마운트됐을 때 이 함수가 실행됨
-      console.log("unmount!");
-    };
-  }, []);
-  return <div>unmount testing component</div>;
-};
+// const Unmount = () => {
+//   // *** Lifecycle 컴포넌트가 언마운트됐을 때 -> 화면에서 사라졌을 때
+//   useEffect(() => {
+//     console.log("Mount!");
+//     return () => {
+//       // 함수를 리턴하면 언마운트됐을 때 이 함수가 실행됨
+//       console.log("unmount!");
+//     };
+//   }, []);
+//   return <div>unmount testing component</div>;
+// };
 
-const Lifecycle = () => {
-  //   const [count, setcount] = useState(0);
-  //   const [text, settext] = useState("");
-  const [isvisible, setisvisible] = useState(false);
-  const toggle = () => {
-    setisvisible(!isvisible);
-  };
+// const Lifecycle = () => {
+//   //   const [count, setcount] = useState(0);
+//   //   const [text, settext] = useState("");
+//   const [isvisible, setisvisible] = useState(false);
+//   const toggle = () => {
+//     setisvisible(!isvisible);
+//   };
 
-  //   // *** Lifecycle 컴포넌트가 마운트됐을 때 -> 화면에 생겼을 때
-  //   useEffect(() => {
-  //     console.log("Mount!");
-  //   }, []); // 의존성 배열에 빈 배열을 전달하면 마운트됐을 때 함수 실행 가능
+//   //   // *** Lifecycle 컴포넌트가 마운트됐을 때 -> 화면에 생겼을 때
+//   //   useEffect(() => {
+//   //     console.log("Mount!");
+//   //   }, []); // 의존성 배열에 빈 배열을 전달하면 마운트됐을 때 함수 실행 가능
 
-  //   // *** Lifecycle 컴포넌트가 업데이트됐을 때 -> 리렌더링 됐을 때
-  //   useEffect(() => {
-  //     console.log("Update!");
-  //   }); // 의존성 배열을 전달하지 않으면 업데이트 됐을 때 함수 실행 가능
-  //   useEffect(() => {
-  //     console.log("count Update!");
-  //   }, [count]); // 의존성 배열에 특정 상태를 넣으면 그 상태로 인해
-  //   // 컴포넌트가 업데이트 됐을 때 함수 실행 가능
-  //   useEffect(() => {
-  //     console.log("text Update!");
-  //   }, [text]);
+//   //   // *** Lifecycle 컴포넌트가 업데이트됐을 때 -> 리렌더링 됐을 때
+//   //   useEffect(() => {
+//   //     console.log("Update!");
+//   //   }); // 의존성 배열을 전달하지 않으면 업데이트 됐을 때 함수 실행 가능
+//   //   useEffect(() => {
+//   //     console.log("count Update!");
+//   //   }, [count]); // 의존성 배열에 특정 상태를 넣으면 그 상태로 인해
+//   //   // 컴포넌트가 업데이트 됐을 때 함수 실행 가능
+//   //   useEffect(() => {
+//   //     console.log("text Update!");
+//   //   }, [text]);
 
-  return (
-    <div style={{ padding: 20 }}>
-      {/* {count}
-      <button
-        onClick={() => {
-          setcount(count + 1);
-        }}
-      >
-        +
-      </button>
-      <button
-        onClick={() => {
-          setcount(count - 1);
-        }}
-      >
-        -
-      </button>
-      <div>
-        <input
-          value={text}
-          onChange={(e) => {
-            settext(e.target.value);
-          }}
-        />
-      </div> */}
+//   return (
+//     <div style={{ padding: 20 }}>
+//       {/* {count}
+//       <button
+//         onClick={() => {
+//           setcount(count + 1);
+//         }}
+//       >
+//         +
+//       </button>
+//       <button
+//         onClick={() => {
+//           setcount(count - 1);
+//         }}
+//       >
+//         -
+//       </button>
+//       <div>
+//         <input
+//           value={text}
+//           onChange={(e) => {
+//             settext(e.target.value);
+//           }}
+//         />
+//       </div> */}
 
-      <button onClick={toggle}>on/off</button>
-      {isvisible && <Unmount />}
-    </div>
-  );
-};
+//       <button onClick={toggle}>on/off</button>
+//       {isvisible && <Unmount />}
+//     </div>
+//   );
+// };
 
-export default Lifecycle;
+// export default Lifecycle;
